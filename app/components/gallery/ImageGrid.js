1a44c78c3f6b2127308331add52fd5f242869350
kaushikigupta <kaushikigupta.1127@gmail.com>
2025-10-27 11:12:35 +0530

    fixed image grid

--- app/components/gallery/ImageGrid.js
+++ app/components/gallery/ImageGrid.js
@@ -8,7 +8,7 @@ export default function ImageGrid({ visibleImages }) {
         <div
           key={i}
           className="rounded-[14px] overflow-hidden bg-[#222225] 
-                     shadow-[0_8px_20px_rgba(0,0,0,0.5)] h-[170px] w-[167px]
+                     shadow-[0_8px_20px_rgba(0,0,0,0.5)] h-[150px] w-[150px]
                      transition-transform duration-700 ease-[cubic-bezier(0.25,0.1,0.25,1)]
                      hover:[transform:perspective(800px)_rotateY(10deg)_rotateX(-3deg)_translateY(-5px)]
                      hover:shadow-[0_12px_28px_rgba(0,0,0,0.6)] "
